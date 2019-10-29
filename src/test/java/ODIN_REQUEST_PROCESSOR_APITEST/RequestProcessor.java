9d65391c333b59b7c21b517a75618fe5e9fcac51
Irimia Ciprian Andrei <ciprian.irimia@metrosystems.net>
2019-10-29 12:30:56 +0200

    tests request processor

--- src/test/java/ODIN_REQUEST_PROCESSOR_APITEST/RequestProcessor.java
+++ src/test/java/ODIN_REQUEST_PROCESSOR_APITEST/RequestProcessor.java
@@ -10,7 +10,9 @@ import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import static ODIN_VALIDATOR_APITEST.Consts.*;
 
@@ -35,6 +37,7 @@ static boolean value = true;
 
 	@BeforeMethod
 	public void startTest() {
+		logger.info("=====================");
 		logger.info("autentification......");
 		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
 		RestAssured.useRelaxedHTTPSValidation();
@@ -43,14 +46,20 @@ static boolean value = true;
 	
 	@AfterMethod
 	public void endTest() throws SQLException {
-		closeDbConn();
+		//closeDbConn();
 		logger.info("logout....");
 		logger.info(" ");
 	}
+	@AfterTest
+	public void closeDB() throws SQLException {
+		closeDbConn();
+
+	}
+
 	
 	@Test (priority=1)
 	public void test1() throws Exception {	
-		logger.info("test for ==>compare xml structure , content , datra inserted in DB vs expected<==");
+		logger.info("test for ==>compare xml structure , content , data inserted in DB vs expected <==");
 		executeQuerryDB(CLEAN_DB, strUserID, strPassword, dbURL);	
 		XmlComparator.read_xml_expected(2, 1,FILEPATH_REQUEST_PROCESSOR, "test_1.xml" , PATH_EXPECTED_REQUEST_PROCESSOR, "expected_1.xml","null",0);
 		listactual = getDatafromDB(executeQuerryDB(GET_DATA_DB,strUserID, strPassword, dbURL));
@@ -63,7 +72,7 @@ static boolean value = true;
 
 	@Test (priority=2)
 	public void test2() throws Exception {
-		logger.info("test for check response ==>http status 200<==");
+		logger.info("test for ==> check response http status 200 <==");
 		
 		Response result = (Response)RestAssured.given().
 				contentType("text/plain").				
@@ -75,7 +84,7 @@ static boolean value = true;
 	
 	@Test (priority=3)
 	public void test3() throws Exception {	
-		logger.info("test for ==>compare xml structure , content , data inserted in DB vs expected<==");
+		logger.info("test for ==><sourceXMLElement><==");
 		executeQuerryDB(CLEAN_DB, strUserID, strPassword, dbURL);	
 		XmlComparator.read_xml_expected(2, 10,FILEPATH_REQUEST_PROCESSOR, "test_1.xml" , PATH_EXPECTED_REQUEST_PROCESSOR, "expected_2.xml","null",0);
 		listactual = getDatafromDB(executeQuerryDB(GET_DATA_DB,strUserID, strPassword, dbURL));
@@ -85,11 +94,10 @@ static boolean value = true;
 		assertEquals(true, value);
 		logger.info("check succesfully");					
 	}
-	
-	
+		
 	@Test (priority=4)
 	public void test4() throws Exception {	
-		logger.info("test for ==>compare xml structure , content , datra inserted in DB vs expected<==");
+		logger.info("test for ==><targetColumn><==");
 		executeQuerryDB(CLEAN_DB, strUserID, strPassword, dbURL);	
 		XmlComparator.read_xml_expected(2, 11,FILEPATH_REQUEST_PROCESSOR, "test_1.xml" , PATH_EXPECTED_REQUEST_PROCESSOR, "expected_3.xml","null",0);
 		listactual = getDatafromDB(executeQuerryDB(GET_DATA_DB,strUserID, strPassword, dbURL));
