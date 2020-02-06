d8b8832a00ca6f00dbfa3a309ce29c42e4c62373
Ciprian Irimia <irimiacip@yahoo.com>
2020-02-06 15:27:37 +0200

    asd

--- src/main/java/ODIN_VALIDATOR_APITEST/DataBaseConsts.java
+++ src/main/java/ODIN_VALIDATOR_APITEST/DataBaseConsts.java
@@ -10,12 +10,10 @@ public class DataBaseConsts {
 	public static final String INSERT_DB_3 = "INSERT INTO odin_test ( COLOANA1,COLOANA2,COLOANA3,COLOANA4)  VALUES ( 1 , 'femeie' ,'alexandra', 28 )";
 	public static final String INSERT_DB_4 = "INSERT INTO odin_test ( COLOANA1,COLOANA2,COLOANA3,COLOANA4)  VALUES ( 1 , 'femeie' ,'alexandra', 28 )";
 	public static final String INSERT_DB_5 = "INSERT INTO odin_test ( COLOANA1,COLOANA2,COLOANA3,COLOANA4)  VALUES ( 2 , 'femeie' ,'alex', 28 )";
-
 	public static final String INSERT_DB_6 = "insert into odin_test (COLOANA1,COLOANA2,COLOANA3,COLOANA4) values (1,'salam sibiu','20-12-2019',200)";
 	public static final String INSERT_DB_7 = "insert into odin_test (COLOANA1,COLOANA2,COLOANA3,COLOANA4) values (2,'salam victoria','20-12-2019',300)";
 	public static final String INSERT_DB_8 = "insert into odin_test (COLOANA1,COLOANA2,COLOANA3,COLOANA4) values (3,'salam agricola','20-12-2019',500)";
 	public static final String INSERT_DB_9 = "insert into odin_test (COLOANA1,COLOANA2,COLOANA3,COLOANA4) values (4,'salam agricola','20-12-2019',400)";
-
 	public static final String dbURL = "jdbc:oracle:thin:@(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=metrom1-scan.de1.ocm.s1587589.oraclecloudatcustomer.com)(PORT=1521)))(CONNECT_DATA=(SERVICE_NAME=MDP07PLD.de1.ocm.s1587589.oraclecloudatcustomer.com)))";
 	public static final String dbURL_pp = "jdbc:oracle:thin:@i125serv.metro-dus.de:15125:MD01PLI";
 	// public static final String strUserID = "nwe";
