5f91e90dc09b6c5c9a151dca0c8377f4c1f68792
Ciprian Irimia <irimiacip@yahoo.com>
2020-02-06 15:30:00 +0200

    ggg

--- src/main/java/ODIN_VALIDATOR_APITEST/Consts.java
+++ src/main/java/ODIN_VALIDATOR_APITEST/Consts.java
@@ -34,13 +34,8 @@ public class Consts {
 	
 	
 	public static final String COUNTRY_TENANT = "PL/MCC/SDA";
-
 	public static final String FILEPATH_VALIDATOR = "src/main/resources/validator/files/";
-	
 	public static final String PATH_EXPECTED_VALIDATOR = "src/main/resources/validator/expected/";
-	
 	public static final String FILEPATH_REQUEST_PROCESSOR = "src/main/resources/process_request/files/";
-
-	
 	public static final String PATH_EXPECTED_REQUEST_PROCESSOR = "src/main/resources/process_request/expected/";
 }
